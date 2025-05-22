716c3e094b44deb20294437cee442d6e936217c7
sandesh ghoti <sandesh.ghoti@gmail.com>
2025-05-23 00:28:07 +0530

    add dashboard nav item and logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
 import { NavLink } from 'react-router';
-import { isLoggedIn, useAppSelector } from '../store';
+import { BACKEND_URL } from '../config';
+import { isLoggedIn, logout, useAppDispatch, useAppSelector } from '../store';
 
 const LOGO_URL = 'https://10xers.co/images/Logo.png';
+const LOGOUT_URL = `${BACKEND_URL}/user/logout`;
 const Navbar = () => {
   const isUserLoggedIn = useAppSelector(isLoggedIn);
+  const dispatch = useAppDispatch();
+  const handleLogout = async () => {
+    dispatch(logout());
+    await fetch(LOGOUT_URL, { method: 'POST', credentials: 'include' });
+    window.alert('logout success');
+  };
   return (
     <>
       <nav className='border-gray-200 bg-white dark:bg-gray-900'>
@@ -14,19 +22,30 @@ const Navbar = () => {
           <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
             <ul className='mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900'>
               <li>
-                <a
-                  href='/'
+                <NavLink
+                  to='/'
                   className='block rounded-sm bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:text-white md:dark:text-blue-500'
                   aria-current='page'
                 >
                   Home
-                </a>
+                </NavLink>
               </li>
               {isUserLoggedIn && (
                 <li>
                   <NavLink
-                    to='/logout'
+                    to='/dashboard'
                     className='block rounded-sm px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500'
+                  >
+                    Dashboard
+                  </NavLink>
+                </li>
+              )}
+              {isUserLoggedIn && (
+                <li>
+                  <NavLink
+                    to='/'
+                    className='block rounded-sm px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500'
+                    onClick={handleLogout}
                   >
                     Logout
                   </NavLink>
