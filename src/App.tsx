0df84fc8e3e8c456beffb4e95c99b15878c5d893
sandesh ghoti <sandesh.ghoti@gmail.com>
2025-05-22 17:50:12 +0530

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { useEffect } from 'react';
 import { Outlet } from 'react-router';
-import { BACKEND_URL } from './config';
-import { useFetch } from './customHooks/useFetch';
+import Navbar from './components/Navbar';
 
 function App() {
-  const { isLoading, data, error, message } = useFetch(BACKEND_URL + '/product', { method: 'GET' });
-  useEffect(() => {
-    console.log('isLoading', isLoading);
-    console.log('data', data);
-    console.log('error', error);
-    console.log('message', message);
-  }, [isLoading, data, error, message]);
   return (
     <>
       <div className='relative flex min-h-screen w-screen justify-center bg-white text-black dark:bg-gray-800 dark:text-white'>
         <div className='max-container mx-auto w-full'>
+          <Navbar />
           <Outlet />
         </div>
       </div>
